ef5c15c00f3e231791d418f12c56910d32efa474
Superman <703353105@qq.com>
2018-05-31 13:11:12 +0800

    first commit

---
+++ SAlertView/UIAlertController+NewVersion.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertController+NewVersion.h
+//  SAlertView
+//
+//  Created by Superman on 2018/5/31.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (NewVersion)
+
+@end
---
+++ SAlertView/UIAlertController+NewVersion.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertController+NewVersion.m
+//  SAlertView
+//
+//  Created by Superman on 2018/5/31.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "UIAlertController+NewVersion.h"
+
+@implementation UIAlertController (NewVersion)
+
+@end
